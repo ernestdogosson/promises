d37d1169dee9eceb938fa62ddc77bcd7babfca6a
Ernest Dogo <dogoernest@outlook.com>
2026-02-10 19:07:51 +0100

    Add joke fetch promise with delayed punchline

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,29 @@ const drink = () => {
       if (Math.random() < 0.5) {
         resolve("You get a drink");
       } else {
-        reject("Out of luck. Try again.");
+        reject("Out of stock. Try again.");
       }
     }, 1000);
   });
 };
 
-drink().then(console.log).catch(console.log);
+// drink().then(console.log).catch(console.log);
+
+const jokes = (id: number) => {
+  const promise = fetch("https://official-joke-api.appspot.com/random_joke")
+    .then((response) => response.json())
+    .then((data) => {
+      let jokeSetup = data.setup;
+      console.log(jokeSetup);
+
+      setTimeout(() => {
+        const jokePunchline = data.punchline;
+        console.log(jokePunchline);
+      }, 5000);
+    })
+    .catch((error) => {
+      console.log(`Error fetching joke: ${error}.`);
+    });
+};
+
+jokes(1);
